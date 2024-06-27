9bfd553030a78595a441d53a4031d6313eb669a0
Vincent Dettmer <88632912+vdettmer@users.noreply.github.com>
2024-06-27 14:29:36 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Semi-manual openssl, create CSR from scratch
+=========
+
+Simple PoC script in Python to create certificate signing requests fast in an automated manner where automations like ACME are not present and CSR have still to be manually reviewed by someone responsible for the CA
+
+How to use?
+------------
+
+Set your unit's default metadata in defaults.yaml.
+Then set the FQDN (cn) and altnames for your requested certificates in csrs.yaml, take a look at the example.
+Make sure modules yaml and os are present in your python env and as well that openssl is installed on your host. Execute the script.
+The usual applies: Send the CSR to the PKI folks responsible for your CA, keep the key for yourself and make sure you don't lose it, wait for your certificate to arrive...
---
+++ csrs.py
@@ -0,0 +1,62 @@
+import yaml
+import os
+
+file = "csrs.yaml"
+defaults = "defaults.yaml"
+
+def load_default_variables():
+  with open(defaults, 'r') as file:
+    vars = yaml.safe_load_all(file)
+    for var in vars:
+      global C
+      global ST
+      global L
+      global O
+      global OU
+      global emailAddress
+      C = str(var['C'])
+      ST = str(var['ST'])
+      L = str(var['L'])
+      O = str(var['O'])
+      OU = str(var['OU'])
+      emailAddress = str(var['emailAddress'])
+
+def generate_conf(file):
+  with open(file, 'r') as file:
+    csrs = yaml.safe_load_all(file)
+    for csr in csrs:
+      name = str(csr['cn'])
+      alt = csr['alt']
+      os.mkdir(name)
+      f = open(name+"/"+name+".conf", "w")
+      f.write('[req]\ndistinguished_name = req_distinguished_name\nreq_extensions = v3_req\nprompt = no\n\n[req_distinguished_name]\nC = '+C+'\nST = '+ST+'\nL = '+L+'\nO = '+O+'\nOU = '+OU+'\nCN = '+name+'\nemailAddress = '+emailAddress+'\n\n[v3_req]\nkeyUsage = keyEncipherment, dataEncipherment\nextendedKeyUsage = serverAuth\nsubjectAltName = @alt_names\n\n[alt_names]\n')
+      f.close
+      i = 0
+      for altname in alt:
+        i = i+1
+        f = open(name+"/"+name+".conf", "a")
+        f.write("DNS."+str(i)+" = "+altname+"\n")
+        f.close
+
+def generate_rsa(file):
+  with open(file, 'r') as file:
+    keys = yaml.safe_load_all(file)
+    for key in keys:
+      name = str(key['cn'])
+      os.system("openssl genrsa -out "+name+"/"+name+".key 4096")
+
+def create_csr(file):
+    with open(file, 'r') as file:
+      csrs = yaml.safe_load_all(file)
+      for csr in csrs:
+        name = str(csr['cn'])
+        os.system("openssl req -new -out "+name+"/"+name+".csr -key "+name+"/"+name+".key -config "+name+"/"+name+".conf")
+
+def main():
+  load_default_variables()
+  generate_conf(file)
+  generate_rsa(file)
+  create_csr(file)
+
+if __name__ == "__main__":
+    main()
